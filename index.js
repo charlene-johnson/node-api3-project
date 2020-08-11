ab9730181c692d6801b541fd0293ab72c61e8a9e
charlene-johnson <charlene.j8234@gmail.com>
2020-08-11 17:37:56 -0400

    Created a logger middleware and added it to index.js, also created the server port

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code away!
+const express = require("express")
+const logger = require("./middleware/logger")
+
+
+const server = express()
+const port = 5000
+
+server.use(express.json())
+
+// Custom middleware
+server.use(logger())
+
+
+
+server.listen(port, () => {
+    console.log(`Sever running at http://localhost:${port}`)
+})
